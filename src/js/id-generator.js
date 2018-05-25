e462ea9339a0d15b21648746294cf5bb855e28ae
Yauheni Dubouski <joinhud@gmail.com>
2018-05-25 17:57:54 +0300

    simple-todo/feature: start implementing view logic

--- src/js/id-generator.js
+++ src/js/id-generator.js
@@ -2,5 +2,8 @@ let idCounter = Number(window.localStorage.getItem('id-counter')) || 0;
 const mask = Date.now();
 
 export default function generateId() {
-  return (++idCounter + mask).toString(16);
+  const id = (++idCounter + mask).toString(16);
+  window.localStorage.setItem('id-counter', idCounter);
+
+  return id;
 }
--- src/js/main-view.js
+++ src/js/main-view.js
@@ -6,8 +6,30 @@ export default class MainView {
   initialize() {
     $('new-task-form').bind('submit', (event) => {
       event.preventDefault();
-
-      this.taskStorageController.addNewTask($('new-task-form task-description').val());
+      const description = $('new-task-form task-description').val();
+      this.taskStorageController.addNewTask(description);
+      refillTaskList();
     });
   }
+}
+
+function createTaskElement(task) {
+  const $taskElement = $('<li class="list-group-item task"></li>');
+  const $taskCompletedBtn = $('<input class="task-completed-btn mx-2" type="checkbox">').prop('checked', task.isCompleted());
+  const $taskDescription = $('<p class="task-description"></p>').text(task.getDescription());
+  const $deleteTaskBtn = $('<button type="button" class="close align-self-start delete-task-btn" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
+
+  $($taskElement).append(taskCompletedBtn, taskDescription, deleteTaskBtn);
+  
+  return $taskElement;
+}
+
+function refillTaskList() {
+  $('#task-list').empty();
+
+  const tasks = this.taskStorageController.findAll();
+  tasks.forEach(task => {
+    const taskElement = createTaskElement(task);
+    $('#task-list').append(taskElement);
+  });
 }
